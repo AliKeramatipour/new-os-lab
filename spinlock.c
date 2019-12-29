75705922eeb4c53f996d672d379c63d4cf252ad0
nima <nima.mg@gmail.com>
2019-12-29 22:28:18 +0330

    releasing spinlock sets owner to -a

--- spinlock.c
+++ spinlock.c
@@ -51,7 +51,7 @@ release(struct spinlock *lk)
 
   lk->pcs[0] = 0;
   lk->cpu = 0;
-
+  lk->owner = -1;
   // Tell the C compiler and the processor to not move loads or stores
   // past this point, to ensure that all the stores in the critical
   // section are visible to other cores before the lock is released.
